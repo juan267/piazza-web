e17eb9c7cf4b203137d4301290919f174f99309c
Juan gomez <j.gomez267@gmail.com>
2024-03-28 17:05:03 -0500

    add tests

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,25 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      @organization = Organization.create(members: [@user])
+
+      redirect_to root_path, 
+        flash: { success: t(".welcome", name: @user.name) },
+        status: :see_other
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password)
+    end
+end
